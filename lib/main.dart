07c36538db482d96b191b3ff6b071c554f290f44
Le An <anlv.it.vn@gmail.com>
2019-10-16 16:32:08 +0700

    Commited

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:climate/screens/loading_screen.dart';
 
-
-
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,15 +1,62 @@
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
+import 'package:climate/services/location.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+const apiKey = '5cfee8afdd24be4d600e2f0c6b1080fa';
+
 class LoadingScreen extends StatefulWidget {
   @override
   _LoadingScreenState createState() => _LoadingScreenState();
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
-  void getYourLocation () async{
-    Position position = await Geolocator().getCurrentPosition(desiredAccuracy: LocationAccuracy.lowest);
-    print(position);
+  double lat;
+  double long;
+
+  void getYourLocation() async {
+    Location location = Location();
+    await location.getLocation();
+    lat = location.latitude;
+    long = location.longitude;
+    getData();
   }
+
+  void getData() async {
+    String data;
+    String url =
+        'https://api.openweathermap.org/data/2.5/forecast?lat=$lat&lon=$long&appid=$apiKey';
+
+    http.Response response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      data = response.body;
+      //print(data);
+
+      var jsonData = jsonDecode(data);
+
+      var temp = jsonData['list'][0]['main']['temp'];
+      var tempMin = jsonData['list'][0]['main']['temp_min'];
+      var tempMax = jsonData['list'][0]['main']['temp_max'];
+      //var city = jsonData['name'];
+      //var id = jsonData['weather'][0]['id'];
+
+      //print('ID : $id');
+      //print('City : $city');
+      print('Tempature : $temp');
+      print('Tempature : $tempMin');
+      print('Tempature : $tempMax');
+    } else {
+      print(response.statusCode);
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getYourLocation();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,7 +64,8 @@ class _LoadingScreenState extends State<LoadingScreen> {
         child: RaisedButton(
           onPressed: () {
             //Get the current location
-            getYourLocation();
+            //getYourLocation();
+            //getData();
           },
           child: Text('Get Location'),
         ),
